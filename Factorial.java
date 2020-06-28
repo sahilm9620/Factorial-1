1e8fb3e251b7fac7891be6c86c9b29d4787e2108
sahilm9620 <65106833+sahilm9620@users.noreply.github.com>
2020-06-28 16:29:00 +0530

    fix bug of -ve value

--- Factorial.java
+++ Factorial.java
@@ -1,17 +1,21 @@
 import java.util.*;
 public class Factorial {
     public static void main(String[] args) {
-        int result=1;
+  public static void main(String[] args) {
+        int result = 1;
         System.out.println("Enter number : ");
-        Scanner sc= new Scanner(System.in);
-        int input = sc. nextInt();
-        
-        for (;input!=0;input--)
-        {
-            System.out.println("Before : "+result+"     "+"Value : "+input);
-            result=result*input;
-            System.out.println("After : "+result+"      "+"Value : "+input);
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+        if (input >= 0) {
+            for (; input != 0; input--) {
+                System.out.println("Before : " + result + "     " + "Value : " + input);
+                result = result * input;
+                System.out.println("After : " + result + "      " + "Value : " + input);
+            }
+            System.out.println("\nFactorial is : " + result);
+        }else {
+            System.out.println("Enter Positive Value \n ");
         }
-        System.out.println("\nFactorial is : "+result);
     }
 }
